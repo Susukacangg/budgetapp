d56c84ea75bc67960ade9216caaac6561fae593a
zachary <zachary.jobb@tngdigital.com.my>
2026-08-13 16:32:47 +0800

    Remove unused code

--- src/features/accounts/index.ts
+++ src/features/accounts/index.ts
@@ -1,9 +1,7 @@
 export {
     type Account,
-    type AccountDao,
     type AccountType,
     convertAccountFromDao,
-    convertAccountsFromDao
 } from './model.ts'
 export { AccountsPage } from './components/AccountsPage.tsx'
 export { AccountsForm } from './components/AccountsForm.tsx'
