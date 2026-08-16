27dcb435a5206ac1568d199b161b5f0d8b27e9df
zachary <zachary.jobb@tngdigital.com.my>
2026-08-16 17:46:47 +0800

    refactor (Fab): rename css class for fab and dynamically set position based on property

--- src/shared/ui/Fab.tsx
+++ src/shared/ui/Fab.tsx
@@ -1,15 +1,51 @@
-import type { MouseEventHandler } from 'react'
+import type { MouseEventHandler, CSSProperties } from 'react'
+
+const FAB_POSITIONS = ['top-right', 'bottom-right', 'top-left', 'bottom-left'] as const
+type FabPosition = (typeof FAB_POSITIONS)[number]
 
 type FabProps = {
   onClick: MouseEventHandler<HTMLButtonElement>
+  position: FabPosition
+  margin: number
 }
 
-export function Fab({ onClick }: FabProps) {
+export function Fab({ onClick, position='bottom-right', margins=2 }: FabProps) {
+
+    function getPositionInset(position: string): CSSProperties {
+        let margin: string = `${margins}rem`
+
+        switch (position) {
+            case 'top-right':
+                return {
+                    top: margin,
+                    right: margin,
+                }
+            case 'bottom-right':
+                return {
+                    right: margin,
+                    bottom: margin,
+                }
+            case 'top-left':
+                return {
+                    top: margin,
+                    left: margin,
+                }
+            case 'bottom-left':
+                return {
+                    bottom: margin,
+                    left: margin,
+                }
+            default:
+                return {}
+        }
+    }
+
     return (
         <button
             type="button"
-            className="round-btn hover-btn add-btn"
+            className="round-btn fab"
             onClick={onClick}
+            style={getPositionInset(position)}
         >
             <svg width="20" height="20" viewBox="0 0 20 20" aria-hidden="true">
                 <path
