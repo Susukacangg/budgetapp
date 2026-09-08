a8fd1d80ad08ae9c14feab16242131d82609e9e9
zachary <zachary.jobb@tngdigital.com.my>
2026-09-08 16:52:16 +0800

    suppress error

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -22,6 +22,7 @@ export function RouteView({ route }: Readonly<RouteViewProps>) {
     case AppRoutes.REPORTS:
       return <ReportsPage />
     default: {
+      // @ts-expect-error Other routes that are not defined will throw an error
       const _exhaustive: never = route
       return _exhaustive
     }
