d3d944888b92abfe57c7b311ef4ffa356c53279f
zachary <zachary.jobb@tngdigital.com.my>
2026-09-09 11:25:44 +0800

    refactor (ListItem): refactor properties and added style property

--- src/shared/ui/ListItem.tsx
+++ src/shared/ui/ListItem.tsx
@@ -1,16 +1,20 @@
 import type {CSSProperties, ReactNode} from 'react'
 
 export type ListItemProps = {
-  index: number,
-  clickable: boolean,
-  children: ReactNode
+    index?: number,
+    clickable?: boolean,
+    children?: ReactNode,
+    style?: CSSProperties,
 }
 
-export function ListItem({index, clickable, children}: Readonly<ListItemProps>) {
+export function ListItem({index=0, clickable=true, children, style}: Readonly<ListItemProps>) {
   return (
       <li
           className={`list-item ${clickable ? 'clickable' : ''}`}
-          style={{'--item-index': index} as CSSProperties}
+          style={{
+              '--item-index': index,
+              ...style
+          } as CSSProperties}
       >
         {children}
       </li>
